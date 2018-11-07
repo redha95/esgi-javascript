7d5c7676c157a67414e8cfef2dcce8a39fd6d8a5
unknown <redha955@hotmail.fr>
2018-11-07 15:45:23 +0100

    exercice1

--- exercice-1/string.js
+++ exercice-1/string.js
@@ -1,8 +1,9 @@
 
 
 
-var ucfirst = function(){
+function ucfirst(){
     var helloworld = "helloworld";
     console.log(helloworld.charAt(0).toUpperCase() + helloworld.slice(1));
     
 }
+ucfirst();
