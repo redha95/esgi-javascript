8a0587d7f8aa8c11f31e02d0141de3efd203e5ec
unknown <redha955@hotmail.fr>
2018-11-07 15:54:21 +0100

    exercice1

--- exercice-1/string.js
+++ exercice-1/string.js
@@ -2,8 +2,31 @@
 
 
 function ucfirst(){
-    var helloworld = "helloworld";
+    var helloworld = "hello world";
     console.log(helloworld.charAt(0).toUpperCase() + helloworld.slice(1));
     
 }
 ucfirst();
+
+function capitalize(){
+    var text = "hello world";
+text = text.toLowerCase()
+    .split(' ')
+    .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+    .join(' ');
+console.log(text);
+}
+capitalize();
+
+function snake_case(){
+    var str = 'hello world !';
+    str = str.replace(/\s+/g, '_');
+    console.log(str);
+}
+snake_case();
+
+function leet(){
+
+
+}
+leet();
