9a61782893eba7e4f466b2748c81c33d9ef55901
unknown <redha955@hotmail.fr>
2018-11-27 12:44:07 +0100

    type_check_v2
    Signed-off-by: unknown <redha955@hotmail.fr>

--- exercice-2/type-check.js
+++ exercice-2/type-check.js
@@ -20,3 +20,26 @@ function type_check_v1(arg1,arg2){
   return false;
 };
 
+function type_check_v2(arg1,objet){
+
+   if(arg1 == objet.value){
+       if(Array.isArray(arg1) === true){
+        if( objet.type == "array"){
+            return true;
+        }
+        return false;
+    }
+    else if(typeof arg1 == "object"){
+        if(objet.type == null && objet.type == "null"){
+            return true;
+        } else if(objet.type == "object" && arg1 != null){
+            return true;
+        }
+        return false;
+    }
+    else if(typeof arg1 == objet.type){
+        return true;
+    }
+   }
+   return false;
+};
