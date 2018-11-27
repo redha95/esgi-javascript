795884ce32432ff5bb86fe81566c441ab95093e5
unknown <redha955@hotmail.fr>
2018-11-27 10:37:58 +0100

    gestion type null
    Signed-off-by: unknown <redha955@hotmail.fr>

--- exercice-2/type-check.js
+++ exercice-2/type-check.js
@@ -3,6 +3,9 @@ function type_check_v1(arg1,arg2){
     if(Array.isArray([arg1]) == true && arg2 == "array"){
         return true;
     }
+    else if(typeof arg1 == "object" && arg2 == "null"){
+        return true;
+    }
     else if(typeof arg1 == arg2){
         return true;
     }
