91eb5b9455f1bc63def68c24210098aa18b6c6d8
unknown <redha955@hotmail.fr>
2018-11-27 10:32:09 +0100

    gestion type array
    Signed-off-by: unknown <redha955@hotmail.fr>

--- exercice-2/type-check.js
+++ exercice-2/type-check.js
@@ -1,6 +1,9 @@
 function type_check_v1(arg1,arg2){
 
-    if(typeof arg1 == arg2){
+    if(Array.isArray([arg1]) == true && arg2 == "array"){
+        return true;
+    }
+    else if(typeof arg1 == arg2){
         return true;
     }
   return false;
