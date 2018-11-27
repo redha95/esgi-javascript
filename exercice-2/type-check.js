d5a5c33f79774e0c533d050d8d26724df024afb5
unknown <redha955@hotmail.fr>
2018-11-27 11:11:57 +0100

    gestion type object
    Signed-off-by: unknown <redha955@hotmail.fr>

--- exercice-2/type-check.js
+++ exercice-2/type-check.js
@@ -1,6 +1,6 @@
 function type_check_v1(arg1,arg2){
 
-    if(Array.isArray([arg1]) == true && arg2 == "array"){
+    if(Array.isArray([arg1]) == true){
         if(arg2 != "array"){
             return false
         } else {
@@ -8,7 +8,6 @@ function type_check_v1(arg1,arg2){
         }
         
     }
-
     else if(typeof arg1 == "object" && arg2 == "null"){
         return true;
     }
