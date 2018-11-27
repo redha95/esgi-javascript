3f64d09517905e3c68ae1d7b6c37e38484f2f0fd
unknown <redha955@hotmail.fr>
2018-11-27 10:42:24 +0100

    gestion type arrayNotobject
    Signed-off-by: unknown <redha955@hotmail.fr>

--- exercice-2/type-check.js
+++ exercice-2/type-check.js
@@ -3,6 +3,9 @@ function type_check_v1(arg1,arg2){
     if(Array.isArray([arg1]) == true && arg2 == "array"){
         return true;
     }
+    else if(Array.isArray([arg1]) == true && arg2 == "object"){
+        return false;
+    }
     else if(typeof arg1 == "object" && arg2 == "null"){
         return true;
     }
