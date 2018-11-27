0ed1e5e51d91be6d6ad21753402ac0cfdd56fbfc
unknown <redha955@hotmail.fr>
2018-11-27 11:50:35 +0100

    gestion type
    Signed-off-by: unknown <redha955@hotmail.fr>

--- exercice-2/type-check.js
+++ exercice-2/type-check.js
@@ -1,19 +1,17 @@
 function type_check_v1(arg1,arg2){
 
-    if(Array.isArray([arg1]) == true){
-        if(arg2 != "array"){
-            return false
-        } else {
-            return false;
-        }
-        
+    if(Array.isArray([arg1]) == true && arg2 == "array"){
+        return true;
+    }
+    else if(Array.isArray([arg1]) === true && arg2 == "object"){
+        return false;
     }
     else if(typeof arg1 == "object" && arg2 == "null"){
         return true;
     }
     else if(typeof arg1 == arg2){
         return true;
-    }  
+    }
   return false;
 };
 
